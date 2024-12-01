e79ead90597bad288a00459b20be43e26d478ab1
alisa_tumanskaia <alisa_tumanskaia@practice.relex.ru>
2024-12-01 05:09:10 +0300

    feature/ added claim page

--- frontend/src/components/confirm-page/ConfirmPage.tsx
+++ frontend/src/components/confirm-page/ConfirmPage.tsx
@@ -6,12 +6,31 @@ import {useLocation} from "react-router";
 
 export const ConfirmPage = () => {
     const location = useLocation();
-    const { documentId}= location.state || {}; //
+    const {documentId} = location.state || {};
+
+    const userPhone = '+71234567890'
+    const formatPhoneNumber = (phone: string) => {
+        return `+7 ··· ··· ${(String(phone)).slice(-4, -2)} ${phone.slice(-2)}`;
+    }
+
+
+    // return (
+    //     <Flex justify={"center"}>
+    //
+    //         <h1>Введите код из сообщения</h1>
+    //         код отправлен на {formatPhoneNumber}
+    //         <CodeInput fields={
+    //             useIsMobile() ? 4 : 5} breakpoint={32}/>
+    //     </Flex>
+    // );
+    //
 
     return (
-        <Flex justify={"center"}>
-            <CodeInput fields={
-                useIsMobile() ? 4 : 5} breakpoint={32}/>
+        <Flex justify={"center"} dir={"column"}>
+
+            {formatPhoneNumber(userPhone)}
+            <CodeInput fields={useIsMobile() ? 4 : 5} breakpoint={32}/>
+
         </Flex>
     );
 };
